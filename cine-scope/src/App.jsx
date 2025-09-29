ccb0d5e5f0bc8ed1b897d064a2a2df99632cacb3
DinethSheveen <fdineth4000@gmail.com>
2025-09-29 23:24:26 +0100

    Imported the navbar component

--- cine-scope/src/App.jsx
+++ cine-scope/src/App.jsx
@@ -1,24 +1,13 @@
+import Navbar from "./assets/Components/Navbar"
 import "./App.css"
-import axios from "axios"
-import React, { useEffect } from 'react'
+
 
 const API_KEY = "7c669c55";
 const API_URL = "http://www.omdbapi.com/?apikey=7c669c55"
 
 function App() {
-  useEffect(()=>{
-    const fetch = async()=>{
-      const response = await axios.get(`${API_URL}&s=superman`)
-      console.log(response.data.Search)
-    }    
-
-    fetch()
-  },[])
-  
-  
-  
   return (
-    <div>App</div>
+    <Navbar/>
   )
 }
 
