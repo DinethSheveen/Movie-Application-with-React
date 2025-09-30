d5e4cddf047bc3b35aa3214b6018670731b04a8a
DinethSheveen <fdineth4000@gmail.com>
2025-09-30 12:42:09 +0100

    Rendered an api call to get the movies

--- cine-scope/src/Movies.jsx
+++ cine-scope/src/Movies.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
 import "./Movies.css"
 import heroImg from "./assets/images/hero.webp"
 import { LiaSearchengin } from "react-icons/lia";
 
 function Movies() {
+  const API_URL = "http://www.omdbapi.com/?apikey=7c669c55"
+  const [movies,setMovies] = useState([])
+
+  useEffect(()=>{
+    const fetchMovies = async() => {
+      const response = await axios.get(`${API_URL}&s=superman`)
+      setMovies(response.data.Search);
+    }
+    fetchMovies()
+  },[]) 
+
+  const draft = movies[0]
+  console.log(draft);
+  
+
   return (
     <div>
         {/* HERO SECTION */}
@@ -16,6 +32,23 @@ function Movies() {
             <div className="input-controls">
                 <input type="text" placeholder='Search though 1000 of movies...'/>
                 <button>Search <LiaSearchengin className='search-icon'/></button>
+            </div>
+
+            {/* MOVIES SECTION */}
+            <div className="movies">
+              {/* MOVIES GRID */}
+              <div className="movies-grid">
+                <div className="movie-card">
+                  <img src={draft.Poster} alt="" />
+                  <img src={draft.Poster} alt="" />
+                  <img src={draft.Poster} alt="" />
+                  <img src={draft.Poster} alt="" />
+                  <img src={draft.Poster} alt="" />
+                  <img src={draft.Poster} alt="" />
+                  <img src={draft.Poster} alt="" />
+                  
+                </div>
+              </div>
             </div>
         </section>
     </div>
