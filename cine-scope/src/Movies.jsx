eb0ff09121e365024eea063bd10352b3b3b7443b
DinethSheveen <fdineth4000@gmail.com>
2025-09-30 10:22:33 +0100

    Main section of the application

---
+++ cine-scope/src/Movies.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Movies.css"
+import heroImg from "./assets/images/hero.webp"
+import { LiaSearchengin } from "react-icons/lia";
+
+function Movies() {
+  return (
+    <div>
+        {/* HERO SECTION */}
+        <div className="hero">
+            <img src={heroImg} alt="" />
+        </div>
+
+        {/* MAIN SECTION */}
+        <section id="main">
+            <div className="input-controls">
+                <input type="text" />
+                <button>Search <LiaSearchengin/></button>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Movies
