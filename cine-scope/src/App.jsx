f12976815aacf9e1b0f530ace49822205017f568
DinethSheveen <fdineth4000@gmail.com>
2025-10-02 17:02:55 +0100

    Imported and rendered the footer

--- cine-scope/src/App.jsx
+++ cine-scope/src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./assets/Components/Navbar/Navbar"
 import "./App.css"
 import Movies from "./Movies"
+import Footer from "./assets/Components/Footer/Footer"
 
 function App() {
   return (
     <>
       <Navbar/>
       <Movies/>
+      <Footer/>
     </>
   )
 }
