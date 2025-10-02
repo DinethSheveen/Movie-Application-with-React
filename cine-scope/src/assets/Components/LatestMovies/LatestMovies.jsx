4ea6c97ec86c5097ccc66e98306671b43b428845
DinethSheveen <fdineth4000@gmail.com>
2025-10-02 14:56:00 +0100

    Added a component to hold the latest movies

---
+++ cine-scope/src/assets/Components/LatestMovies/LatestMovies.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./LatestMovies.css"
+
+function LatestMovies() {
+  return (
+    <div id="latest-movies" className='latest-movies-section'>
+        {/* Heading */}
+        <h2 className='latest-movies-header'>Latest Shows</h2>
+    </div>
+  )
+}
+
+export default LatestMovies
