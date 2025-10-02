a045c6d0cfb84be27314976c26c6b58c875d2506
DinethSheveen <fdineth4000@gmail.com>
2025-10-02 16:21:12 +0100

    Removed unnecessary imports and rendering

--- cine-scope/src/App.jsx
+++ cine-scope/src/App.jsx
@@ -1,14 +1,12 @@
 import Navbar from "./assets/Components/Navbar/Navbar"
 import "./App.css"
 import Movies from "./Movies"
-import LatestMovies from "./assets/Components/LatestMovies/LatestMovies"
 
 function App() {
   return (
     <>
       <Navbar/>
       <Movies/>
-      <LatestMovies/>
     </>
   )
 }
