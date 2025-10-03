36bf84ff7e5f4bb445b1557a1c2c5caba51756fc
DinethSheveen <fdineth4000@gmail.com>
2025-10-03 23:26:53 +0100

    Imported and rendered TrendingMoves component

--- cine-scope/src/App.jsx
+++ cine-scope/src/App.jsx
@@ -2,12 +2,14 @@ import Navbar from "./assets/Components/Navbar/Navbar"
 import "./App.css"
 import Movies from "./Movies"
 import Footer from "./assets/Components/Footer/Footer"
+import Trending from "./assets/Components/TrendingMovies/Trending"
 
 function App() {
   return (
     <>
       <Navbar/>
       <Movies/>
+      <Trending/>
       <Footer/>
     </>
   )
